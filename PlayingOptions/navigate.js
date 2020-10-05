56e19fd360a13263fe51d5e8e144b93e9ae9ab51
jpena91@gmail.com <jpena91@gmail.com>
2020-10-05 02:40:02 -0400

    move repos/ first commit

---
+++ PlayingOptions/navigate.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+var request = require("request"); // "Request" library
+
+const router = express.Router();
+
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
+
+router.post("/pause", (req, res) => {
+  console.log(req.headers.token);
+  var options = {
+    url: "https://api.spotify.com/v1/me/player/pause",
+    headers: {
+      Authorization: "Bearer " + req.headers.token,
+    },
+    json: true,
+  };
+
+  // use the access token to access the Spotify Web API
+  request.put(options, function (error, response, body) {
+    if (response.statusCode === 204) {
+      res.send({ status: 200, message: "Paused" });
+    } else {
+      res.send({
+        status: 400,
+        message: "There was an error completing you request, please try again.",
+      });
+    }
+  });
+});
+
+module.exports = router;
